6bc8bc1bede521244d244ae9d383bd7ab542376f
Your Name <you@example.com>
2019-04-01 13:40:05 -0400

    docs

--- docs/template/visualize.py
+++
@@ -1,8 +0,0 @@
-import sklearn
-import tasks
-
-
-def accuracy():
-    model = tasks.TaskTrain().output().load()
-    df_train = tasks.TaskPreprocess().output().load()
-    print(sklearn.metrics.accuracy_score(df_train['y'],model.predict(df_train.iloc[:,:-1])))
