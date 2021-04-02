7498f8a65a981fc71d9eb6890a529f46bafae49d
d6tdev <support@databolt.tech>
2021-04-02 11:48:01 -0400

    utest

--- tests/tmp.py
+++ tests/tmp.py
@@ -1,10 +1,5 @@
-import pandas as pd
 import d6tflow
-import logging
-
-import sys
-log = logging.getLogger()
-logging.basicConfig(stream=sys.stdout, level=logging.INFO)
+import pandas as pd
 
 
 # define 2 tasks that load raw data
@@ -13,24 +8,26 @@ class Task1(d6tflow.tasks.TaskCache):
     def run(self):
         df = pd.DataFrame({'a': range(3)})
         self.save(df)  # quickly save dataframe
+        self.metaSave({'a': 1})
 
 
+@d6tflow.requires(Task1)
 class Task2(Task1):
     pass
 
 
-# @d6tflow.requires({0: Task1, 1: Task2})
-@d6tflow.requires(Task1, Task2)
+# define another task that depends on data from task1 and task2
+@d6tflow.requires(Task2)
 class Task3(d6tflow.tasks.TaskCache):
     multiplier = d6tflow.IntParameter(default=2)
 
     def run(self):
-        df1 = self.input()[0].load()  # quickly load input data
-        df2 = self.input()[1].load()  # quickly load input data
+        df1 = self.input()['upstream1'].load()  # quickly load input data
+        df2 = self.input()['upstream2'].load()  # quickly load input data
         df = df1.join(df2, lsuffix='1', rsuffix='2')
         df['b'] = df['a1'] * self.multiplier  # use task parameter
         self.save(df)
+        print(self.metaLoad()['a'])
+        self.saveMeta({'b': 2})
 
-
-# Execute task including all its dependencies
 d6tflow.run(Task3())
