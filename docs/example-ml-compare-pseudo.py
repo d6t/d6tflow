4439468ea8ec1548c22b532f5ae4e0649fee6c55
d6tdev <support@databolt.tech>
2021-04-28 00:27:05 -0400

    Update example-ml-compare-pseudo.py

--- docs/example-ml-compare-pseudo.py
+++ docs/example-ml-compare-pseudo.py
@@ -16,42 +16,40 @@ class ModelTrain(d6tflow.tasks.TaskPickle):
     def run(self):
         data = self.inputLoad() # quickly load training data
 
-        if self.model=='regression':
+        if self.model=='regression': # choose model to train
             # [...]
         elif self.model=='deep learning':
             # [...]
 
         model.train(data)
-        self.save(model)
-        self.saveMeta({'score':model.score})
+        self.save(model)  # save trained model
+        self.saveMeta({'score':model.score})  # save additional experiment data
 
 # goal: compare performance of two models
 # define workflow manager
-flow = d6tflow.WorkflowMulti(ModelTrain, \
+flow = d6tflow.WorkflowMulti(ModelTrain, 
         {'model1':{'model':'regression'}, 'model2':{'model':'deep learning'}})
 
 # intelligently figures out which part of the workflow need to run for each model
 # for example when training model 2, GetData() does not need to run again
 flow.run()
 '''
-Scheduled 3 tasks of which:
-* 3 ran successfully:
+Scheduled 2 tasks of which:
+* 2 ran successfully:
     - 1 GetData()
-    - 1 ModelData(do_preprocess=True)
-    - 1 ModelTrain(do_preprocess=True, model=ols)
+    - 1 ModelTrain(model=regression)
 
 # To run 2nd model, don't need to re-run all tasks, only the ones that changed
-Scheduled 3 tasks of which:
+Scheduled 2 tasks of which:
 * 1 complete ones were encountered:
     - 1 GetData()
-* 2 ran successfully:
-    - 1 ModelData(do_preprocess=False)
-    - 1 ModelTrain(do_preprocess=False, model=gbm)
+* 1 ran successfully:
+    - 1 ModelTrain(model=deep learning)
 
 '''
 
-data = flow.outputLoadAll()
-
 scores = flow.outputLoadMeta()
-print(scores)
-# {'ols': {'score': 0.7406426641094095}, 'gbm': {'score': 0.9761405838418584}}
+# {'ols': {'regression': 0.7406426641094095}, 'deep learning': {'score': 0.9761405838418584}}
+
+data = flow.outputLoad(task=GetData)  # quickly load training data
+models = flow.outputLoad(task=ModelTrain)  # quickly load trained models
